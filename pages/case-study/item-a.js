437e7665081b00f2b3654056006bc807bf027e0b
Chris Miller <miller.t.chris@gmail.com>
2021-05-28 10:28:20 -0400

    Refactored the portfolio items.

--- pages/case-study/item-a.js
+++ pages/case-study/item-a.js
@@ -1,5 +1,4 @@
 import Hero from "../../components/Hero";
-import ContentG from "../../components/ContentG";
 import Gallery from "../../components/Gallery";
 import FeaturedImage from "../../components/FeaturedImage";
 import Layout from "../../components/layout/Layout";
@@ -11,169 +10,167 @@ export default function caseStudy() {
     <>
       <Header theme="white" logo="/bloom_logo_white.svg" />
 
-      <Layout>
-        <Hero
-          logo="/logo-majid-white.svg"
-          logoAltText="Logo"
-          title="Majid Al Futtaim Advanced Analytics"
-          background="https://source.unsplash.com/random"
-          backgroundAltText="Background Image"
-        />
-        <BasicContent style="style-1">
-          <div className="col content">
-            <h2 class="title">Lorem, ipsum dolor.</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae impedit provident consequatur, nobis nihil voluptatum
-              porro eum officiis? Maxime, laboriosam?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-          </div>
-          <div className="col list">
-            <h2 className="title">Project services</h2>
-            <ul>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-              <li>Lorem, ipsum dolor.</li>
-            </ul>
-          </div>
-        </BasicContent>
-        <FeaturedImage image="/notes-grid.svg" />
-        <BasicContent style="style-2">
-          <div className="col headline">
-            <h2 class="title">Lorem, ipsum dolor.</h2>
-          </div>
-          <div className="col content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae impedit provident consequatur, nobis nihil voluptatum
-              porro eum officiis? Maxime, laboriosam?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-          </div>
-        </BasicContent>
-        <BasicContent style="style-2">
-          <div className="col headline">
-            <h2 class="title">Lorem, ipsum dolor.</h2>
-          </div>
-          <div className="col content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae impedit provident consequatur, nobis nihil voluptatum
-              porro eum officiis? Maxime, laboriosam?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-          </div>
-        </BasicContent>
+      <Hero
+        logo="/logo-majid-white.svg"
+        logoAltText="Logo"
+        title="Majid Al Futtaim Advanced Analytics"
+        background="https://source.unsplash.com/random"
+        backgroundAltText="Background Image"
+      />
+      <BasicContent style="style-1">
+        <div className="col content">
+          <h2 class="title">Lorem, ipsum dolor.</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            impedit provident consequatur, nobis nihil voluptatum porro eum
+            officiis? Maxime, laboriosam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+        </div>
+        <div className="col list">
+          <h2 className="title">Project services</h2>
+          <ul>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+            <li>Lorem, ipsum dolor.</li>
+          </ul>
+        </div>
+      </BasicContent>
+      <FeaturedImage image="/notes-grid.svg" />
+      <BasicContent style="style-2">
+        <div className="col headline">
+          <h2 class="title">Lorem, ipsum dolor.</h2>
+        </div>
+        <div className="col content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            impedit provident consequatur, nobis nihil voluptatum porro eum
+            officiis? Maxime, laboriosam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+        </div>
+      </BasicContent>
+      <BasicContent style="style-2">
+        <div className="col headline">
+          <h2 class="title">Lorem, ipsum dolor.</h2>
+        </div>
+        <div className="col content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            impedit provident consequatur, nobis nihil voluptatum porro eum
+            officiis? Maxime, laboriosam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+        </div>
+      </BasicContent>
 
-        <Gallery>
-          <div className="col image">
-            <img
-              src="https://source.unsplash.com/random"
-              alt="Place holder image"
-            />
-          </div>
-          <div className="col image">
-            <img
-              src="https://source.unsplash.com/random"
-              alt="Place holder image"
-            />
-          </div>
-          <div className="col image">
-            <img
-              src="https://source.unsplash.com/random"
-              alt="Place holder image"
-            />
-          </div>
-          <div className="col image">
-            <img
-              src="https://source.unsplash.com/random"
-              alt="Place holder image"
-            />
-          </div>
-        </Gallery>
+      <Gallery>
+        <div className="col image">
+          <img
+            src="https://source.unsplash.com/random"
+            alt="Place holder image"
+          />
+        </div>
+        <div className="col image">
+          <img
+            src="https://source.unsplash.com/random"
+            alt="Place holder image"
+          />
+        </div>
+        <div className="col image">
+          <img
+            src="https://source.unsplash.com/random"
+            alt="Place holder image"
+          />
+        </div>
+        <div className="col image">
+          <img
+            src="https://source.unsplash.com/random"
+            alt="Place holder image"
+          />
+        </div>
+      </Gallery>
 
-        <BasicContent style="style-2">
-          <div className="col headline">
-            <h2 class="title">Lorem, ipsum dolor.</h2>
-          </div>
-          <div className="col content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Recusandae impedit provident consequatur, nobis nihil voluptatum
-              porro eum officiis? Maxime, laboriosam?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-              facilis architecto nemo laboriosam libero laudantium officiis
-              ipsum doloribus, natus repudiandae?
-            </p>
-          </div>
-        </BasicContent>
-      </Layout>
+      <BasicContent style="style-2">
+        <div className="col headline">
+          <h2 class="title">Lorem, ipsum dolor.</h2>
+        </div>
+        <div className="col content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            impedit provident consequatur, nobis nihil voluptatum porro eum
+            officiis? Maxime, laboriosam?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            facilis architecto nemo laboriosam libero laudantium officiis ipsum
+            doloribus, natus repudiandae?
+          </p>
+        </div>
+      </BasicContent>
     </>
   );
 }
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,65 +1,76 @@
-import PortfolioLayout from "../components/PortfolioLayout";
 import PortfolioItem from "../components/PortfolioItem";
-import Layout from "../components/layout/Layout";
 import Header from "../components/Header";
-import Link from "next/link";
 
 export default function portfolio() {
   return (
     <>
       <Header theme="black" logo="/bloom_logo_black.svg" />
-      <Layout>
-        <PortfolioLayout headline="The work we do">
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-          <PortfolioItem
-            image="https://source.unsplash.com/random"
-            title="Lorem ipsum dolor"
-            text="This is a description of the post that is featured in this post item here."
-            link="/case-study"
-          />
-        </PortfolioLayout>
-      </Layout>
+
+      <section className="block portfolio">
+        <div className="component-wrapper">
+          <div className="row intro">
+            <h1 class="title">The word we do.</h1>
+          </div>
+          <div className="row items">
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+            <PortfolioItem
+              image="https://source.unsplash.com/random"
+              imageAlt="Place holder image"
+              title="Lorem ipsum dolor"
+              description="This is a description of the post that is featured in this post item here."
+              link="/case-study"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
