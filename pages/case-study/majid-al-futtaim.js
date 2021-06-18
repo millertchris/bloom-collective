6cde5f101690a41bfb042cb34e910d0fecc49aa2
Matt Ooley <matt@bloomcollective.com>
2021-06-18 14:53:06 +0700

    matt edits

--- pages/case-study/majid-al-futtaim.js
+++ pages/case-study/majid-al-futtaim.js
@@ -26,21 +26,38 @@ export default function caseStudy() {
             Covid-19
           </h2>
           <p>
-            In January 2020, none of us knew what was coming. Not one of us knew
-            the world was about to change forever. Not you, not me. Not clients,
-            and not agencies.
+            <em>
+              First and foremost, it should be said that while working through a
+              pandemic is not something we would want to experience again – we
+              are grateful to Majid Al Futtaim for the opportunity to help them
+              communicate during this difficult time. They provided us with the
+              information we needed, the patience required, and the crucial
+              timely feedback necessary to do this work. Without their
+              cooperation, we couldn't have done it. So, hats off to amazing
+              clients everywhere!
+            </em>
           </p>
           <p>
-            When the news about Covid-19 broke, Bloom Collective had recently
-            started working alongside Majid Al Futtaim as their internal comms
-            content agency.
+            <b>In January 2020,</b> the world was a different place as we had
+            yet to hear about Covid-19. Coincidentally, during this time Bloom
+            Collective was humbled to begin working alongside Majid Al Futtaim
+            as their internal comms content agency. We were already knee-deep in
+            planning and strategy for the upcoming year when news of Covid-19
+            broke. As the world began to grind to a halt, we knew we had to
+            pivot quickly to help MAF communicate with their nearly 45,000
+            employees spread across 16 countries.
           </p>
           <p>
-            As the pandemic progressed and the world began to close its doors
-            and borders, we knew we had to act fast to help Majid Al Futtaim
-            communicate with their nearly 45,000 employees.
+            Around the middle of February and as the pandemic steadily
+            progressed, governments across the Middle East and North Africa
+            (MENA) began imposing shutdowns across public spaces including
+            malls, cinemas, hotels and more. This resulted in the majority of
+            Majid Al Futtaim's staff being forced to work from home – a practice
+            that was uncommon in the Middle East at the time. While Covid made
+            most everything uncertain, one thing was clear: communication would
+            be crucial in maintaining any sense of normalcy across the
+            organization.
           </p>
-          <p>This is how we did it.</p>
         </div>
         <div className="col list">
           <h2 className="title">Project services</h2>
@@ -107,25 +124,32 @@ export default function caseStudy() {
         </div>
         <div className="col content">
           <p>
-            At the onset of the pandemic the only certainty was uncertainty. The
-            world had changed and it was palpable. Enitre nations were
-            grappeling with how to cope. There was no precedent for how to run a
-            multinational admidst a global virus outbreak, but as an
-            organization, Majid Al Futtaim quickly sprung in to crisis
-            leadership mode.
+            As Majid Al Futtaim's internal comms partner, Bloom Collective was
+            in a unique position to co-create, coordinate and put in motion
+            their groupwide internal response; We worked closely with their
+            internal leadership through the uncertainty of the pandemic to do
+            just that.
           </p>
           <p>
-            At the end of February, governments across the Middle East and North
-            Africa (MENA) began imposing shutdowns across public spaces
-            including malls, cinemas, hotels and more. This resutled in the
-            majority of MAF staff being forced to work from home – a practice
-            that was uncommon in the Middle East at the time.
+            Since there was no blueprint for how to run a multinational crisis
+            response to a rapidly evolving global pandemic, we had to create one
+            scratch. It was critical that MAF's internal comms were aligned in
+            messaging while also being effective and agile in getting
+            information to employees in real time. With a clear mandate from the
+            beginning that employee safety and wellness was the top priority,
+            our first step was to devise Covid-specific content categories to
+            drive effective engagement around relevant messaging.
           </p>
           <p>
-            As Majid Al Futtaim's corporate communications creative content
-            agency, Bloom Collective was in a unique position to co-create and
-            co-ordinate the organization's group-wide internal response. We
-            worked closely with their internal leadership team to do just that.
+            To adapt to the rapidly evolving Covid news cycle, Bloom Collective
+            adjusted our working schedule to align with that of our client and
+            introduced a 24/7 around-the-clock response. This allowed us to
+            rapidly address concerns being raised through the feedback
+            mechanisms introduced as part of MAF's wider internal communications
+            crisis strategy. This also gave us the flexibility to provide an
+            always-on drumbeat of relevant and timely content for an
+            increasingly anxious audience. We were able to produce content on
+            demand as the situation progressed.
           </p>
         </div>
       </BasicContent>
@@ -157,50 +181,30 @@ export default function caseStudy() {
         </div>
         <div className="col content">
           <p>
-            As the organization began to implement measures in line with
-            government mandated safety protocols, the workforce became almost
-            entirely relient on internal digital channels for business updates,
-            Covid news and need-to-know information. With a clear mandate that
-            employee safety and wellness was the top priorty, our first step was
-            to devise Covid-specific content categories to drive effective
-            engagement around the crisis strategy.
+            Once our workflow was fully up and running, we effectively doubled
+            the amount of content flowing through MAF's online channels. In a
+            period of just over four months we created over 150 custom
+            animations, GIFs, Newsers and social poster content for MAF's
+            digital internal social networks. Much of this content was also
+            replicated and cascaded through local offline and other external
+            channels owned by MAF – such as inside of Carrefour grocery stores
+            and other MAF properties.
           </p>
           <p>
-            Bloom Collective adjusted our team's work-from-home hours and
-            introduced a 24/7 newsroom-style content workflow. Together with the
-            client, this enabled us to ensure we stayed on top of the fast-pace
-            of global information and quickly evolving local government
-            recommendations.
+            Thrice weekly check-in surveys measured overall sentiment throughout
+            the pandemic. MAF scored +11 on global benchmarks set out to measure
+            how staff perceived their clarity of communication and overall
+            handling during Covid. This is, no doubt, due to the comms plan put
+            together in cooperation between Majid Al Futtaim and Bloom
+            Collective.
           </p>
           <p>
-            It also allowed us to address concerns being raised through the
-            feedback mechanisms introduced as part of Majid Al Futtaim's wider
-            internal communications crisis strategy. This gave us the
-            flexibility to provide an always-on drumbeat of relevant and timely
-            content for an increasingly anxious audience.
+            <a href="/contact">Contact Bloom Collective</a> to see how we can
+            help your content aspirations.
           </p>
         </div>
       </BasicContent>
 
-      <BasicContent style="style-2">
-        <div className="col headline">
-          <h2 class="title">Plan in motion</h2>
-        </div>
-        <div className="col content">
-          <p>
-            In a period of just over four months we created over 150 custom
-            animations, GIFs and social poster content for MAF's internal
-            communications channels.
-          </p>
-          <p>
-            Our content covered four main categories - covid safety content,
-            covid news content, corporate updates and entertainment content.
-            This allowed us to have a breadth of incoming information to produce
-            on a daily basis. Content was also replicated and cascased through
-            offline and external channels.
-          </p>
-        </div>
-      </BasicContent>
       <Footer />
     </>
   );
