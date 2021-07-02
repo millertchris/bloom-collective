5b5c207f7b552549f7e4aa424a5aa62c5d669778
patricksoe1 <patrick.soe@hotmail.com>
2021-07-02 11:22:39 +0700

    Refactor portfolio page to user carousels for content

--- pages/portfolio/breakthrough-prize-social-media-content.js
+++ pages/portfolio/breakthrough-prize-social-media-content.js
@@ -5,6 +5,14 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import BasicContent from "../../components/BasicContent";
 
+import Flickity from "react-flickity-component";
+
+const flickityOptions = {
+  wrapAround: true,
+  initialIndex: 0,
+  contain: true,
+};
+
 export default function caseStudy() {
   return (
     <>
@@ -50,204 +58,259 @@ export default function caseStudy() {
           </ul>
         </div>
       </BasicContent>
-      <Gallery columns="two-column">
-        <div className="col video">
-          <div className="videoWrapper">
-            <iframe
-              src="https://www.youtube.com/embed/wrHEK0VtHWQ"
-              title="Breakthrough Prize 2019 Im Memoriam Video"
-              modestbranding="1"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            ></iframe>
-          </div>
+      <section className="block carousel-block">
+        <div className="component-wrapper">
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate={true} // default false
+            static={true} // default false
+          >
+            <div className="col video">
+              <div className="videoWrapper">
+                <iframe
+                  src="https://www.youtube.com/embed/wrHEK0VtHWQ"
+                  title="Breakthrough Prize 2019 Im Memoriam Video"
+                  modestbranding="1"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                ></iframe>
+              </div>
+            </div>
+            <div className="col video">
+              <div className="videoWrapper">
+                <iframe
+                  src="https://www.youtube.com/embed/HVU2huYtVeg"
+                  title="Jocelyn Bell Burnell Special Breakthrough Prize Video"
+                  modestbranding="1"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                ></iframe>
+              </div>
+            </div>
+            <div className="col video">
+              <div className="videoWrapper">
+                <iframe
+                  src="https://www.youtube.com/embed/HLJva6ss2Bc"
+                  title="Breakthrough Initiatives MeerKAT Video"
+                  modestbranding="1"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                ></iframe>
+              </div>
+            </div>
+            <div className="col video">
+              <div className="videoWrapper">
+                <iframe
+                  src="https://www.youtube.com/embed/zlzs6AoIOJ0"
+                  title="Event Horizon (EHT) Telescope Collaboration Video"
+                  modestbranding="1"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                ></iframe>
+              </div>
+            </div>
+          </Flickity>
         </div>
-        <div className="col video">
-          <div className="videoWrapper">
-            <iframe
-              src="https://www.youtube.com/embed/HVU2huYtVeg"
-              title="Jocelyn Bell Burnell Special Breakthrough Prize Video"
-              modestbranding="1"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            ></iframe>
-          </div>
+      </section>
+      <section className="block carousel-block">
+        <div className="component-wrapper">
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate={true} // default false
+            static={true} // default false
+          >
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/2-breakthrough-prize-laureate-james-allison.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/1-breakthrough-prize-laureate-stephen-hawking.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/3-breakthrough-prize-laureate-cumrun-vafa.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/4-breakthrough-prize-laureate-emmanuelle-charpenteir.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/5-breakthrough-prize-laureate-stephen-elledge.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/breakthrough-prize-laureates/6-breakthrough-prize-laureate-robert-langer.jpg"
+                alt="Place holder image"
+              />
+            </div>
+          </Flickity>
         </div>
-        <div className="col video">
-          <div className="videoWrapper">
-            <iframe
-              src="https://www.youtube.com/embed/HLJva6ss2Bc"
-              title="Breakthrough Initiatives MeerKAT Video"
-              modestbranding="1"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            ></iframe>
-          </div>
+      </section>
+      <section className="block carousel-block">
+        <div className="component-wrapper">
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate={true} // default false
+            static={true} // default false
+          >
+            <div className="col image">
+              <img
+                src="/women-in-stem/1-women-in-stem-katherine-johnson.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/women-in-stem/2-women-in-stem-maryam-mirzakani.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/women-in-stem/3-women-in-stem-valentina-tereshkova.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/women-in-stem/4-women-in-stem-florence-bascom.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/women-in-stem/5-women-in-stem-hattie-elizabeth-alexander.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/women-in-stem/6-women-in-stem-marie-maynard-daily.jpg"
+                alt="Place holder image"
+              />
+            </div>
+          </Flickity>
         </div>
-        <div className="col video">
-          <div className="videoWrapper">
-            <iframe
-              src="https://www.youtube.com/embed/zlzs6AoIOJ0"
-              title="Event Horizon (EHT) Telescope Collaboration Video"
-              modestbranding="1"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
-            ></iframe>
-          </div>
+      </section>
+      <section className="block carousel-block">
+        <div className="component-wrapper">
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate={true} // default false
+            static={true} // default false
+          >
+            <div className="col image">
+              <img
+                src="/on-this-day/1-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/on-this-day/2-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/on-this-day/3-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/on-this-day/4-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/on-this-day/5-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/on-this-day/6-on-this-day-in-science.jpg"
+                alt="Place holder image"
+              />
+            </div>
+          </Flickity>
         </div>
-      </Gallery>
-      <Gallery columns="three-column">
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/2-breakthrough-prize-laureate-james-allison.jpg"
-            alt="Place holder image"
-          />
+      </section>
+      <section className="block carousel-block">
+        <div className="component-wrapper">
+          <Flickity
+            className={"carousel"} // default ''
+            elementType={"div"} // default 'div'
+            options={flickityOptions} // takes flickity options {}
+            disableImagesLoaded={true} // default false
+            reloadOnUpdate={true} // default false
+            static={true} // default false
+          >
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/1-scientist-birthday-nikola-tesla.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/2-scientist-birthday-steven-weinberg.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/3-scientist-birthday-albert-einstein.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/4-scientist-birthday-irene-fischer.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/5-scientist-birthday-heinrich-lenz.jpg"
+                alt="Place holder image"
+              />
+            </div>
+            <div className="col image">
+              <img
+                src="/scientist-birthdays/6-scientist-birthday-srinivasa-ramanujan.jpg"
+                alt="Place holder image"
+              />
+            </div>
+          </Flickity>
         </div>
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/1-breakthrough-prize-laureate-stephen-hawking.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/3-breakthrough-prize-laureate-cumrun-vafa.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/4-breakthrough-prize-laureate-emmanuelle-charpenteir.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/5-breakthrough-prize-laureate-stephen-elledge.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/breakthrough-prize-laureates/6-breakthrough-prize-laureate-robert-langer.jpg"
-            alt="Place holder image"
-          />
-        </div>
-      </Gallery>
-      <Gallery columns="three-column">
-        <div className="col image">
-          <img
-            src="/women-in-stem/1-women-in-stem-katherine-johnson.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/women-in-stem/2-women-in-stem-maryam-mirzakani.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/women-in-stem/3-women-in-stem-valentina-tereshkova.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/women-in-stem/4-women-in-stem-florence-bascom.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/women-in-stem/5-women-in-stem-hattie-elizabeth-alexander.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/women-in-stem/6-women-in-stem-marie-maynard-daily.jpg"
-            alt="Place holder image"
-          />
-        </div>
-      </Gallery>
-      <Gallery columns="three-column">
-        <div className="col image">
-          <img
-            src="/on-this-day/1-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/on-this-day/2-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/on-this-day/3-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/on-this-day/4-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/on-this-day/5-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/on-this-day/6-on-this-day-in-science.jpg"
-            alt="Place holder image"
-          />
-        </div>
-      </Gallery>
-      <Gallery columns="three-column">
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/1-scientist-birthday-nikola-tesla.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/2-scientist-birthday-steven-weinberg.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/3-scientist-birthday-albert-einstein.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/4-scientist-birthday-irene-fischer.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/5-scientist-birthday-heinrich-lenz.jpg"
-            alt="Place holder image"
-          />
-        </div>
-        <div className="col image">
-          <img
-            src="/scientist-birthdays/6-scientist-birthday-srinivasa-ramanujan.jpg"
-            alt="Place holder image"
-          />
-        </div>
-      </Gallery>
+      </section>
       <Footer />
     </>
   );
