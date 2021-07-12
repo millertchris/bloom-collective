93cca6cb87e29b2ef39786b0ce8b0a45b82cc7de
patricksoe1 <patrick.soe@hotmail.com>
2021-07-12 16:10:56 +0700

    Add portfolio logos and change client logo

--- pages/portfolio/lenovo-product-launch-animation.js
+++ pages/portfolio/lenovo-product-launch-animation.js
@@ -11,8 +11,8 @@ export default function caseStudy() {
       <Header theme="white" logo="/bloom_logo_white.svg" />
 
       <Hero
-        style="style-2"
-        logo="/logo-majid-white.svg"
+        style="style-2 style-6"
+        logo="/logo-lenovo-white.svg"
         logoAlt="Logo"
         title="Lenovo Product Launch Animation"
         imageAlt="Background Image"
